3120d8bc17037d86eea8c1b47d5e60790d29ea2b
minamo <ph.minamo@cytisan.com>
2012-09-08 13:10:11 +0800

    Add vsdoc and minified js. Improve build utilities.

--- pack-nuget.bat
+++ pack-nuget.bat
@@ -1,7 +1,14 @@
 @echo off
 
+set w8=jquery-w8-1.8.1.js
+set min=jquery-w8-1.8.1.min.js
+
+echo minifying with uglifyjs...
+call uglifyjs -o %min% %w8%
+call utils\UTF8BOM -f .
+
 mkdir nuget\Content
 mkdir nuget\Content\Scripts
 
 copy jquery-w8-*.js nuget\Content\Scripts
-NuGet.exe pack nuget\Package.nuspec
+call utils\NuGet.exe pack nuget\Package.nuspec
