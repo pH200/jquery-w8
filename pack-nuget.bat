97fa76e5876700d2d5fd472570270fb03b23aa8e
minamo <ph.minamo@cytisan.com>
2012-09-22 02:52:59 +0800

    Update to 1.8.2. UTF8BOM now normalizes CRLF lines.

--- pack-nuget.bat
+++ pack-nuget.bat
@@ -1,12 +1,13 @@
 @echo off
 
-set w8=jquery-w8-1.8.1.js
-set min=jquery-w8-1.8.1.min.js
+set w8=jquery-w8-1.8.2.js
+set min=jquery-w8-1.8.2.min.js
 
 echo minifying with uglifyjs...
 call uglifyjs -o %min% %w8%
-call utils\UTF8BOM -f .
+call utils\UTF8BOM -f --crlf .
 
+if exist nuget\Content\NUL rmdir /s /q nuget\Content
 mkdir nuget\Content
 mkdir nuget\Content\Scripts
 
